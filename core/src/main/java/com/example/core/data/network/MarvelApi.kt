e927dceb9c3c93563c68fc111853b7984f82b241
ednaldofranco <ednaldo.franco@picpay.com>
2022-05-08 20:42:15 -0300

    criando estrutura de enjeção de dependência

--- core/src/main/java/com/example/core/data/network/MarvelApi.kt
+++ core/src/main/java/com/example/core/data/network/MarvelApi.kt
@@ -1,11 +1,12 @@
 package com.example.core.data.network
 
 import com.example.core.data.network.response.DataWrapperResponse
-import jdk.nashorn.internal.objects.annotations.Getter
 import retrofit2.Call
+import retrofit2.http.GET
 import retrofit2.http.QueryMap
 
 interface MarvelApi {
-    @Getter(name = "characters")
+
+    @GET("characters")
     suspend fun getCharacters(@QueryMap queries: Map<String, String>): Call<DataWrapperResponse>
 }
